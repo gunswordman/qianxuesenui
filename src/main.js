52e4466428f2fbed2ce4868f112e3bc2045f4930
keroro <1030371149@qq.com>
2021-12-19 15:47:30 +0800

    renamed:    src/assets/login_bg .png -> src/assets/login_bg.png

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import './plugins/element.js'
 import './assets/css/global.css'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://47.102.217.120:8000/qxs'
+axios.defaults.baseURL = 'http://127.0.0.1:8000/qxs'
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 
